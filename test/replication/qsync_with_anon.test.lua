f95fd671f3dcbfc4592a7abce44a41af21cece51
Serge Petrenko <sergepetrenko@tarantool.org>
2021-06-09 12:56:40 +0300

    box: introduce `box.ctl.demote`
    Introduce a new journal entry, DEMOTE. The entry has the same meaning as
    PROMOTE, with the only difference that it clears limbo ownership instead
    of transferring it to the issuer.
    
    Introduce `box.ctl.demote`, a counterpart to `box.ctl.promote`, which
    clears the limbo ownership (when elections are off) via writing the DEMOTE, or
    simply makes this instance step down from the leader's role (when elections
    are enabled in any mode).
    
    A new request was necessary instead of simply writing PROMOTE(origin_id
    = 0), because origin_id is deduced from row.replica_id, which cannot be
    0 for replicated rows (it's always equal to instance_id of the row
    originator).
    
    Closes #6034
    
    @TarantoolBot document
    Title: box.ctl.demote
    
    `box.ctl.demote()` is a counterpart to `box.ctl.promote()` which works
    as follows:
     - when `box.cfg.election_mode` is not 'off': make the instance give up
       leadership.
     - when `box.cfg.election_mode` is 'off': write a DEMOTE entry to WAL.
    
    `box.ctl.demote()` may only be issued on the synchronous transaction
    queue owner (i.e. leader when elections are enabled).
    
    A DEMOTE request (DEMOTE = 32) copies PROMOTE behaviour (it clears the
    limbo as well), but clears the synchronous transaction queue  ownership instead
    of assigning it to a new instance.

--- test/replication/qsync_with_anon.test.lua
+++ test/replication/qsync_with_anon.test.lua
@@ -6,6 +6,20 @@ box.schema.user.grant('guest', 'replication')
 box.cfg{replication_synchro_quorum=NUM_INSTANCES}
 _ = box.schema.space.create('sync', {is_sync=true})
 _ = box.space.sync:create_index('pk')
+<<<<<<< HEAD
+=======
+box.ctl.promote()
+-- Testcase body.
+test_run:switch('default')
+box.space.sync:insert{1} -- success
+box.space.sync:insert{2} -- success
+box.space.sync:insert{3} -- success
+test_run:cmd('switch replica_anon')
+box.space.sync:select{} -- 1, 2, 3
+-- Testcase cleanup.
+test_run:switch('default')
+box.space.sync:drop()
+>>>>>>> box: introduce `box.ctl.demote`
 
 
 -- Setup a cluster with anonymous replica
@@ -29,3 +43,13 @@ box.space.sync:drop()
 test_run:cmd('stop server replica_anon')
 test_run:cmd('cleanup server replica_anon')
 test_run:cmd('delete server replica_anon')
+<<<<<<< HEAD
+=======
+box.schema.user.revoke('guest', 'replication')
+box.cfg{                                                                        \
+    replication_synchro_quorum = orig_synchro_quorum,                           \
+    replication_synchro_timeout = orig_synchro_timeout,                         \
+}
+box.ctl.demote()
+test_run:cleanup_cluster()
+>>>>>>> box: introduce `box.ctl.demote`
